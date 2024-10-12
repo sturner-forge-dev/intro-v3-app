77fc924f558e15f753e325b2de7b0ceae1b3ecac
Steven Turner <sturner@forge-applications.io>
2024-10-12 07:14:48 -0500

    getting started

---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,13 @@
+// all files are server components by default
+// this means that we have access to nodejs-type functionality
+// for instance....
+
+// console.log(process)
+
+// returns the {process} object
+
+const SettingsPage = () => {
+  return <div>Settings Page</div>
+}
+
+export default SettingsPage
---
+++ app/docs/[[...id]]/page.tsx
@@ -0,0 +1,7 @@
+const DocsPage = ({ params, searchParams }) => {
+  console.log(params)
+  console.log(searchParams)
+  return <div>Docs Page</div>
+}
+
+export default DocsPage
